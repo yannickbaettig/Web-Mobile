9e041a3e56cdd4c745838588595f0a2dd4aa6a48
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-04-26 22:58:56 +0200

    collision

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const ship = document.getElementById("ship");
 const game = document.getElementById("game");
+const sky = document.getElementById("sky");
+const ground = document.getElementById("ground");
 let timer;
 let updateBeat = 10;
 let positionX = 0;
 let positionY = 0;
+let skyPositionX = 0;
+let groundPositionX = 0;
+const skySpeed = 1;
+const groundSpeed = 2;
 const speed = 6;
 const bulletSpeed = 10;
 let right;
@@ -12,12 +18,13 @@ let up;
 let down;
 let shoot;
 let bullets = [];
+let bulletCounter = 0;
+let enemies = [];
+let enemyCounter = 0;
 
 function startGame(){
     console.log("start Game");
     timer = setInterval(draw,updateBeat);
-    console.log(game.offsetWidth);
-    console.log(game.offsetHeight);
 }
 
 document.onkeydown = function(e) {
@@ -34,11 +41,16 @@ document.onkeyup = function(e) {
     if(e.keyCode === 39 || e.keyCode === 68) right = false;
     if(e.keyCode === 38 || e.keyCode === 87) up = false;
     if(e.keyCode === 40 || e.keyCode === 83) down = false;
-    if(e.keyCode === 32 ) shoot = false;
+    if(e.keyCode === 32 ) {
+        shoot = false;
+        bulletCounter = 0;
+    }
 };
 
 function draw() {
+    collisionBulletsEnemies();
     bullets.forEach(moveBullets);
+    enemies.forEach(moveEnemies);
     if (down) {
         moveDown()
     }
@@ -52,45 +64,107 @@ function draw() {
         moveRight()
     }
     if (shoot) {
-        createBullets();
+        if (bulletCounter % 10 === 0){
+            createBullet();
+        }
+        bulletCounter ++;
     }
+    if (enemyCounter % 50 === 0){
+        createEnemy();
+    }
+    enemyCounter++;
+
     ship.style.transform = `translateX(${positionX}px) translateY(${positionY}px)`;
-    console.log("Left: " + positionX + " Top: "+ positionY);
-    console.log(bullets)
+    moveBackground()
+//    console.log("Left: " + positionX + " Top: "+ positionY);
+//    console.log(bullets)
 }
 
-function createBullets() {
+function collisionBulletsEnemies() {
+    enemies.forEach((enemy)=>{
+        bullets.forEach((bullet) => {
+            if (bullet.mydata.positionX > enemy.mydata.positionX - enemy.offsetWidth &&
+                bullet.mydata.positionY < enemy.mydata.positionY + enemy.offsetHeight &&
+                bullet.mydata.positionY + bullet.offsetHeight >  enemy.mydata.positionY) {
+                let index = bullets.indexOf(bullet);
+                if (index > -1) {
+                    bullets.splice(index, 1);
+                    game.removeChild(bullet);
+                }
+                index = enemies.indexOf(enemy);
+                if (index > -1) {
+                    enemies.splice(index, 1);
+                    game.removeChild(enemy);
+                }
+            }
+        });
+    });
+}
+
+function createBullet() {
     let bullet = document.createElement("div");
+    bullet.mydata = {};
+    bullet.mydata.positionX = positionX + ship.offsetWidth;
+    bullet.mydata.positionY = positionY + ship.offsetHeight/2;
     bullet.className = "bullet";
-    let bulletPositionX = positionX + ship.offsetWidth;
-    let bulletPositionY = positionY + ship.offsetHeight/2;
-    bullet.style.transform = `translateX(${bulletPositionX}px) translateY(${bulletPositionY}px)`
+    bullet.style.transform = `translateX(${bullet.mydata.positionX}px) translateY(${bullet.mydata.positionY}px)`;
     game.appendChild(bullet);
     bullets.push(bullet);
 
 }
 
 function moveBullets(bullet) {
-    let matrix = getTranslate(bullet);
-    let bulletPositionX = matrix.m41 + bulletSpeed;
-    let bulletPositionY = matrix.m42;
-    if (bulletPositionX - bullet.style.width > game.offsetWidth/2) {
+    bullet.mydata.positionX += bulletSpeed;
+    if (bullet.mydata.positionX - bullet.style.width > game.offsetWidth/2) {
         let index = bullets.indexOf(bullet);
         if (index > -1) {
             bullets.splice(index, 1);
             game.removeChild(bullet);
         }
     } else {
-        bullet.style.transform = `translateX(${bulletPositionX}px) translateY(${bulletPositionY}px)`
+        bullet.style.transform = `translateX(${bullet.mydata.positionX}px) translateY(${bullet.mydata.positionY}px)`
     }
 
 }
 
-function getTranslate(object) {
-    let style = window.getComputedStyle(object);
-    return new WebKitCSSMatrix(style.webkitTransform);
+function createEnemy() {
+    let enemy = document.createElement("img");
+    enemy.src = "img/enemy.png";
+    enemy.className = "enemy";
+    enemy.mydata = {};
+    enemy.mydata.positionX = game.offsetWidth/2 + (Math.floor(Math.random() * 400 ) + enemy.offsetWidth);
+    enemy.mydata.positionY = Math.floor((Math.random() * -game.offsetHeight)) + game.offsetHeight/2;
+    enemy.style.transform = `translateX(${enemy.mydata.positionX}px) translateY(${enemy.mydata.positionY}px)`;
+    game.appendChild(enemy);
+    enemies.push(enemy);
+
 }
 
+function moveEnemies(enemy) {
+    enemy.mydata.positionX -= speed;
+    if (enemy.mydata.positionX - enemy.style.width < -game.offsetWidth/2) {
+        let index = enemies.indexOf(enemy);
+        if (index > -1) {
+            enemies.splice(index, 1);
+            game.removeChild(enemy);
+        }
+    } else {
+        enemy.style.transform = `translateX(${enemy.mydata.positionX}px) translateY(${enemy.mydata.positionY}px)`
+    }
+}
+
+function moveBackground() {
+    if (skyPositionX > 19000){
+        skyPositionX = 0;
+    }
+    if (groundPositionX > 19000){
+        groundPositionX = 0;
+    }
+    skyPositionX += skySpeed;
+    groundPositionX += groundSpeed;
+    sky.style.transform = `translateX(${-skyPositionX}px)`
+    ground.style.transform = `translateX(${-groundPositionX}px)`
+}
 
 function moveLeft() {
     if (positionX > (-game.offsetWidth/2)) {
