09b7a3f45931a490dcf207d7b600f02e5a45f678
Bättig Yannick ibbaetti <yannick.baettig@stud.hslu.ch>
2018-05-05 19:12:19 +0200

    game over

--- js/main.js
+++ js/main.js
@@ -2,16 +2,20 @@ const ship = document.getElementById("ship");
 const game = document.getElementById("game");
 const sky = document.getElementById("sky");
 const ground = document.getElementById("ground");
+const startButton = document.getElementById("start");
+const gameOverText = document.getElementById("gameOver");
 let timer;
 let updateBeat = 10;
-let positionX = 0;
-let positionY = 0;
+ship.mydata = {};
+ship.mydata.positionX = 0;
+ship.mydata.positionY = 0;
+ship.mydata.lives = 3;
 let skyPositionX = 0;
 let groundPositionX = 0;
 const skySpeed = 1;
 const groundSpeed = 2;
 const speed = 6;
-const bulletSpeed = 10;
+const bulletSpeed = 6;
 let right;
 let left;
 let up;
@@ -21,10 +25,44 @@ let bullets = [];
 let bulletCounter = 0;
 let enemies = [];
 let enemyCounter = 0;
+let lives  = [];
+let isGameOver = true;
+
 
 function startGame(){
     console.log("start Game");
+    ship.style.display = "block";
+    ship.mydata.positionX = 0;
+    ship.mydata.positionY = 0;
+    startButton.style.display = "none";
+    gameOverText.style.display = "none";
+
+    if (bullets.length !== 0){
+        bullets.forEach(bullet => {
+            game.removeChild(bullet);
+        });
+        bullets = [];
+    }
+    if (enemies.length !== 0){
+        enemies.forEach(enemy => {
+            game.removeChild(enemy);
+        });
+        enemies = [];
+    }
+
+    createLives();
     timer = setInterval(draw,updateBeat);
+
+    isGameOver = false;
+}
+
+
+function gameOver() {
+    ship.style.display = "none";
+    gameOverText.style.display = "block";
+    startButton.style.display = "block";
+    isGameOver = true;
+    clearInterval(timer);
 }
 
 document.onkeydown = function(e) {
@@ -48,64 +86,112 @@ document.onkeyup = function(e) {
 };
 
 function draw() {
-    collisionBulletsEnemies();
-    bullets.forEach(moveBullets);
-    enemies.forEach(moveEnemies);
-    if (down) {
-        moveDown()
-    }
-    if (up) {
-        moveUp()
-    }
-    if (left) {
-        moveLeft()
-    }
-    if (right) {
-        moveRight()
-    }
-    if (shoot) {
-        if (bulletCounter % 10 === 0){
-            createBullet();
+    if (!isGameOver) {
+        collisionBulletsEnemies();
+        collisionPlayerEnemies();
+        if (down) {
+            moveDown()
         }
-        bulletCounter ++;
-    }
-    if (enemyCounter % 50 === 0){
-        createEnemy();
-    }
-    enemyCounter++;
+        if (up) {
+            moveUp()
+        }
+        if (left) {
+            moveLeft()
+        }
+        if (right) {
+            moveRight()
+        }
+        if (shoot) {
+            if (bulletCounter % 10 === 0) {
+                createBullet();
+            }
+            bulletCounter++;
+        }
+        bullets.forEach(moveBullets);
+        enemies.forEach(moveEnemies);
 
-    ship.style.transform = `translateX(${positionX}px) translateY(${positionY}px)`;
-    moveBackground()
-//    console.log("Left: " + positionX + " Top: "+ positionY);
-//    console.log(bullets)
+        if (enemyCounter % 50 === 0) {
+            createEnemy();
+        }
+        enemyCounter++;
+
+        ship.style.transform = `translateX(${ship.mydata.positionX}px) translateY(${ship.mydata.positionY}px)`;
+        moveBackground()
+    }
 }
 
 function collisionBulletsEnemies() {
     enemies.forEach((enemy)=>{
         bullets.forEach((bullet) => {
-            if (bullet.mydata.positionX > enemy.mydata.positionX - enemy.offsetWidth &&
-                bullet.mydata.positionY < enemy.mydata.positionY + enemy.offsetHeight &&
-                bullet.mydata.positionY + bullet.offsetHeight >  enemy.mydata.positionY) {
+            if (collision(bullet, enemy)) {
+                enemy.mydata.lives -= 1;
+
                 let index = bullets.indexOf(bullet);
                 if (index > -1) {
                     bullets.splice(index, 1);
                     game.removeChild(bullet);
                 }
-                index = enemies.indexOf(enemy);
-                if (index > -1) {
-                    enemies.splice(index, 1);
-                    game.removeChild(enemy);
+                if (enemy.mydata.lives === 0) {
+                    let index = enemies.indexOf(enemy);
+                    if (index > -1) {
+                        enemies.splice(index, 1);
+                        game.removeChild(enemy);
+                    }
                 }
             }
         });
     });
 }
 
+
+function collisionPlayerEnemies() {
+    enemies.forEach((enemy) => {
+        if (collision(ship, enemy)) {
+            enemy.mydata.lives = 0;
+            let live = lives.pop();
+            game.removeChild(live);
+            ship.className = "blink";
+            ship.
+            let index = enemies.indexOf(enemy);
+            if (index > -1) {
+                enemies.splice(index, 1);
+                game.removeChild(enemy);
+            }
+            if (lives.length === 0) {
+               gameOver();
+            }
+        }
+    })
+}
+
+function collision(obj1, obj2) {
+    return (obj1.mydata.positionX < obj2.mydata.positionX + obj2.offsetWidth &&
+        obj1.mydata.positionX + obj1.offsetWidth > obj2.mydata.positionX &&
+        obj1.mydata.positionY < obj2.mydata.positionY + obj2.offsetHeight &&
+        obj1.mydata.positionY + obj1.offsetHeight >  obj2.mydata.positionY)
+}
+
+function createLives() {
+    let left = 0;
+    for (let i = 0; i < ship.mydata.lives; i++) {
+        let live = document.createElement("img");
+        live.src = "img/lives.png";
+        live.className = "lives";
+        live.style.left = left + "px";
+        lives.push(live);
+        left += 40;
+    }
+
+    lives.forEach(live => {
+        game.appendChild(live);
+    });
+}
+
 function createBullet() {
     let bullet = document.createElement("div");
     bullet.mydata = {};
-    bullet.mydata.positionX = positionX + ship.offsetWidth;
-    bullet.mydata.positionY = positionY + ship.offsetHeight/2;
+    bullet.mydata.positionX = ship.mydata.positionX + ship.offsetWidth;
+    bullet.mydata.positionY = ship.mydata.positionY + ship.offsetHeight/2;
     bullet.className = "bullet";
     bullet.style.transform = `translateX(${bullet.mydata.positionX}px) translateY(${bullet.mydata.positionY}px)`;
     game.appendChild(bullet);
@@ -114,7 +200,6 @@ function createBullet() {
 }
 
 function moveBullets(bullet) {
-    bullet.mydata.positionX += bulletSpeed;
     if (bullet.mydata.positionX - bullet.style.width > game.offsetWidth/2) {
         let index = bullets.indexOf(bullet);
         if (index > -1) {
@@ -122,6 +207,7 @@ function moveBullets(bullet) {
             game.removeChild(bullet);
         }
     } else {
+        bullet.mydata.positionX += bulletSpeed;
         bullet.style.transform = `translateX(${bullet.mydata.positionX}px) translateY(${bullet.mydata.positionY}px)`
     }
 
@@ -134,6 +220,7 @@ function createEnemy() {
     enemy.mydata = {};
     enemy.mydata.positionX = game.offsetWidth/2 + (Math.floor(Math.random() * 400 ) + enemy.offsetWidth);
     enemy.mydata.positionY = Math.floor((Math.random() * -game.offsetHeight)) + game.offsetHeight/2;
+    enemy.mydata.lives = 2;
     enemy.style.transform = `translateX(${enemy.mydata.positionX}px) translateY(${enemy.mydata.positionY}px)`;
     game.appendChild(enemy);
     enemies.push(enemy);
@@ -141,7 +228,6 @@ function createEnemy() {
 }
 
 function moveEnemies(enemy) {
-    enemy.mydata.positionX -= speed;
     if (enemy.mydata.positionX - enemy.style.width < -game.offsetWidth/2) {
         let index = enemies.indexOf(enemy);
         if (index > -1) {
@@ -149,6 +235,7 @@ function moveEnemies(enemy) {
             game.removeChild(enemy);
         }
     } else {
+        enemy.mydata.positionX -= speed;
         enemy.style.transform = `translateX(${enemy.mydata.positionX}px) translateY(${enemy.mydata.positionY}px)`
     }
 }
@@ -162,31 +249,31 @@ function moveBackground() {
     }
     skyPositionX += skySpeed;
     groundPositionX += groundSpeed;
-    sky.style.transform = `translateX(${-skyPositionX}px)`
-    ground.style.transform = `translateX(${-groundPositionX}px)`
+    sky.style.transform = `translateX(${-skyPositionX}px)`;
+    ground.style.transform = `translateX(${-groundPositionX}px)`;
 }
 
 function moveLeft() {
-    if (positionX > (-game.offsetWidth/2)) {
-        positionX -= speed;
+    if (ship.mydata.positionX > (-game.offsetWidth/2)) {
+        ship.mydata.positionX -= speed;
     }
 }
 
 function moveRight(){
-    if (positionX + ship.offsetWidth < (game.offsetWidth/2)) {
-        positionX += speed;
+    if (ship.mydata.positionX + ship.offsetWidth < (game.offsetWidth/2)) {
+        ship.mydata.positionX += speed;
     }
 }
 
 function moveUp() {
-    if (positionY > (-game.offsetHeight/2)) {
-        positionY -= speed;
+    if (ship.mydata.positionY > (-game.offsetHeight/2)) {
+        ship.mydata.positionY -= speed;
     }
 }
 
 function moveDown() {
-    if (positionY + ship.offsetHeight < (game.offsetHeight/2)) {
-        positionY += speed;
+    if (ship.mydata.positionY + ship.offsetHeight < (game.offsetHeight/2)) {
+        ship.mydata.positionY += speed;
     }
 }
 
